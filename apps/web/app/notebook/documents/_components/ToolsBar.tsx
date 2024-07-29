31d6e8a327eee6a54f02985460820ec2ea91d342
gumingcn <gumingcn@gmail.com>
2024-07-29 15:41:49 +0800

    upload the feature of chat

--- apps/web/app/notebook/documents/_components/ToolsBar.tsx
+++ apps/web/app/notebook/documents/_components/ToolsBar.tsx
@@ -1,22 +1,15 @@
 "use client";
 
 import React from 'react';
+import { nanoid } from '@/lib/utils'
+import Chat from './Chat';
+
 
 const ToolsBar = () => {
-  return (
-    <div className="h-full flex-col p-4 items-center justify-center">
-      <aside className="w-64 bg-gray-800 text-black flex flex-col h-full">
-        <div className="p-4">
-          <h2 className="text-xl font-bold">侧边栏</h2>
-        </div>
-        <nav className="flex-1 overflow-y-auto">
-          <ul className="space-y-2 p-4">
-            <li><a href="#" className="block p-2 bg-gray-700 rounded">链接 1</a></li>
-          </ul>
-        </nav>
-      </aside>
-    </div>
-  );
+
+    return (
+        <Chat />
+      );
 };
 
 export default ToolsBar;
