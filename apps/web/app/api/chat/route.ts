7813b39e64990897576b5f599bf2f185fc49db7f
gumingcn <gumingcn@gmail.com>
2024-07-31 10:20:03 +0800

    upload the feature of chat

--- apps/web/app/api/chat/route.ts
+++ apps/web/app/api/chat/route.ts
@@ -1,5 +1,5 @@
 import { openai } from '@ai-sdk/openai';
-import { convertToCoreMessages, streamText } from 'ai';
+import { StreamData, streamText } from 'ai';
 
 // Allow streaming responses up to 30 seconds
 export const maxDuration = 30;
@@ -7,10 +7,18 @@ export const maxDuration = 30;
 export async function POST(req: Request) {
   const { messages } = await req.json();
 
+  // optional: use stream data
+  const data = new StreamData();
+  data.append('initialized call');
+
   const result = await streamText({
-    model: openai('gpt-3.5-turbo'),
-    messages: convertToCoreMessages(messages),
+    model: openai('gpt-4-turbo'),
+    messages,
+    onFinish() {
+      data.append('call completed');
+      data.close();
+    },
   });
 
-  return result.toAIStreamResponse();
+  return result.toDataStreamResponse({ data });
 }
--- apps/web/app/notebook/documents/_components/Chat.tsx
+++ apps/web/app/notebook/documents/_components/Chat.tsx
@@ -1,25 +1,32 @@
 'use client';
 
-import { useChat } from 'ai/react';
-import React from 'react';
+import { Message, useChat } from 'ai/react';
 
 export default function Chat() {
-  const { messages, input, handleInputChange, handleSubmit } = useChat();
+  const { messages, input, handleInputChange, handleSubmit, data } = useChat({
+    api: '/api/use-chat-streamdata',
+  });
+
   return (
     <div className="flex flex-col w-full max-w-md py-24 mx-auto stretch">
-      {messages.map(m => (
+      {data && (
+        <pre className="p-4 text-sm bg-gray-100">
+          {JSON.stringify(data, null, 2)}
+        </pre>
+      )}
+
+      {messages?.map((m: Message) => (
         <div key={m.id} className="whitespace-pre-wrap">
-          {m.role === 'user' ? 'User: ' : 'AI: '}
+          <strong>{`${m.role}: `}</strong>
           {m.content}
+          <br />
+          <br />
         </div>
       ))}
 
-      <form
-        onSubmit={handleSubmit}
-        className="fixed bottom-0 w-full max-w-md mb-8 border border-gray-300 rounded shadow-xl"
-      >
+      <form onSubmit={handleSubmit}>
         <input
-          className="w-full p-2"
+          className="fixed bottom-0 w-full max-w-md p-2 mb-8 border border-gray-300 rounded shadow-xl"
           value={input}
           placeholder="Say something..."
           onChange={handleInputChange}
